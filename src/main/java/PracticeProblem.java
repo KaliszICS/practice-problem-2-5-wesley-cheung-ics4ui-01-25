25ec292974ca8ccb347179bd2b886f11bb3d50f9
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-10-20 14:37:02 +0000

    update practiceproblem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,19 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static void insertionSort(char[] characters) {
+		char key;
+		int index;
+		for (int i = 1;i<characters.length;i++) {
+			key = characters[i];
+			index = i-1;
+			while (index >= 0 && key < characters[index]) {
+				characters[index+1] = characters[index];
+				index--;
+			}
+			characters[index+1] = key;
+		}
+	}
 
 
 }
